ce01166c27298050bf8a51f35e3c8b9f32504b41
bngo42 <bngo@student.42.fr>
2019-11-16 19:20:35 +0100

    Adding method for creating items boxes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,67 @@
 import { getRequestHeaders } from './auth';
+import moment from 'moment';
+
+let selected = [];
+
+
 
 function listDriveFile() {
-  getRequestHeaders().then(headers => console.log(headers));
+  getRequestHeaders().then(headers => 
+    {
+      let options = {
+        headers
+      };
+
+      fetch('https://www.googleapis.com/drive/v3/files?pageSize=25&fields=files/webViewLink,files/mimeType,files/iconLink,files/thumbnailLink,files/name,files/modifiedTime', options)
+        .then(res => res.json()).then(data => {
+          if (data && data.files) {
+
+            let container = document.querySelector('#content'),
+                folderContainer = document.querySelector('#content-folders');
+            let fileBox = document.querySelector('.file-box');
+
+            
+            data.files.forEach(file => {
+              console.log(file);
+              let newBox = fileBox.cloneNode(true),
+                  title = newBox.querySelector('.title'),
+                  icon = newBox.querySelector('.icon'),
+                  timestamp = newBox.querySelector('.timestamp'),
+                  thumb = newBox.querySelector('.thumb');
+              
+              let itemType = filterMime(file.mimeType);
+              
+              newBox.classList.remove('hidden');
+              icon.style.backgroundImage = ` url(${file.iconLink})`;
+              title.innerText = file.name;
+              timestamp.innerText = `Modified ${moment(file.modifiedTime).fromNow()}`;
+              newBox.addEventListener('click', () => openLink(file.webViewLink));
+
+              if (itemType !== 'folder') {
+                thumb.style.backgroundImage = `url(${file.thumbnailLink || file.iconLink})`;
+                container.appendChild(newBox);
+              } else {
+                thumb.classList.add('hidden');
+                timestamp.classList.add('hidden');
+                folderContainer.appendChild(newBox);
+              }
+
+            });
+          }
+        }).catch(console.error);
+    });
+}
+
+function openLink(link) {
+  window.open(link, '_blank');
+}
+
+function filterMime(mime) {
+  return mime.replace("application/vnd.google-apps.", "");
+}
+
+function toggleItem(item) {
+  console.log(item);
 }
 
 listDriveFile();
